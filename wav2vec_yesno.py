03e1fefea91e92e96fa611acb8c68103bab0b2ea
edwindn <edn.work@proton.me>
2024-12-27 11:38:36 +0000

    Add files via upload

---
+++ wav2vec_yesno.py
@@ -0,0 +1,147 @@
+import numpy as np
+import librosa
+import matplotlib.pyplot as plt
+from scipy.io import wavfile
+from tqdm import tqdm
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+from torchaudio.models import wav2vec2_base
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+wav2vec = wav2vec2_base().to(device)
+processor = wav2vec.feature_extractor
+encoder = wav2vec.encoder
+
+def load_wav_file(file_path):
+    y, sr = librosa.load(file_path, sr=None)
+    return y, sr
+
+yes_data = []
+no_data = []
+sr = 44100
+
+lens = []
+
+for i in range(370):
+    no, _ = load_wav_file(f'no{i}.wav')
+    no = librosa.resample(no, orig_sr=44100, target_sr=16000) # optional for direct model
+    lens.append(no.shape[0])
+    no_data.append((no, 0))
+    yes, _ = load_wav_file(f'yes{i}.wav')
+    yes = librosa.resample(yes, orig_sr=44100, target_sr=16000) # optional for direct model
+    lens.append(yes.shape[0])
+    yes_data.append((yes, 1))
+
+test_yes = []
+test_no = []
+
+for i in range(370, 400):
+    no, _ = load_wav_file(f'no{i}.wav')
+    no = librosa.resample(no, orig_sr=44100, target_sr=16000) # optional for direct model
+    test_no.append((no, 0))
+    yes, _ = load_wav_file(f'yes{i}.wav')
+    yes = librosa.resample(yes, orig_sr=44100, target_sr=16000) # optional for direct model
+    test_yes.append((yes, 1))
+
+data = yes_data + no_data
+
+fixed_len  = 60000
+for i, (d, l) in enumerate(data):
+    if d.shape[0] < fixed_len:
+        d = np.pad(d, (0, fixed_len - d.shape[0]), 'constant')
+    else:
+        d = d[:fixed_len]
+    data[i] = (d, l)
+
+test_data = test_yes + test_no
+fixed_len  = 60000
+for i, (d, l) in enumerate(test_data):
+    if d.shape[0] < fixed_len:
+        d = np.pad(d, (0, fixed_len - d.shape[0]), 'constant')
+    else:
+        d = d[:fixed_len]
+    test_data[i] = (d, l)
+
+data = [(torch.tensor(d, dtype=torch.float32).unsqueeze(0), torch.tensor(l)) for d, l in data]
+test_data = [(torch.tensor(d, dtype=torch.float32).unsqueeze(0), torch.tensor(l)) for d, l in test_data]
+
+
+wav2vec_train = []
+wav2vec_test = []
+for d in data:
+    audio, label = d
+    audio = audio.squeeze().to(device)
+    audio = processor(audio, length=audio.size(1))[0]
+    audio = encoder(audio)
+    wav2vec_train.append((audio, label))
+for d in test_data:
+    audio, label = d
+    audio = audio.squeeze().to(device)
+    audio = processor(audio, length=audio.size(1))[0]
+    audio = encoder(audio)
+    wav2vec_test.append((audio, label))
+
+dataloader = DataLoader(wav2vec_train, shuffle=True, batch_size=4)
+test_dataloader = DataLoader(wav2vec_test, shuffle=True, batch_size=4)
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+
+        self.main = nn.Sequential(
+            nn.Conv2d(1, 16, 8, 4, 2),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Conv2d(16, 16, 8, 4, 2),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Flatten(),
+            nn.Linear(384, 64),
+            nn.ReLU(),
+            nn.Linear(64, 2),
+            nn.Sigmoid()
+        ).to(device)
+    
+    def forward(self, x):
+        x = x.to(device).unsqueeze(1)
+        return self.main(x)
+    
+net = Net()
+optimizer = torch.optim.Adam(net.parameters(), lr=0.0001)
+loss_fn = nn.CrossEntropyLoss()
+
+def train():
+    for epoch in range(20):
+        torch.cuda.empty_cache()
+
+        print(f'Epoch {epoch}')
+        epoch_loss = 0
+        net.train()
+        for batch in tqdm(dataloader):
+            audio, label = batch
+            audio = audio.to(device)
+            label = label.to(device)
+            preds = net(audio)
+            loss = loss_fn(preds, label)
+            net.zero_grad()
+            loss.backward()
+            optimizer.step()
+            epoch_loss += loss.item()
+        print(f'Loss: {epoch_loss / len(dataloader)}')
+        net.main.eval()
+        correct = 0
+        total = 0
+        for batch in test_dataloader:
+            audio, label = batch
+            audio = audio.to(device)
+            label = label.to(device)
+            preds = net(audio)
+            preds = torch.argmax(preds, dim=1)
+            correct += (preds == label).sum().item()
+            total += len(label)
+        print(f'Accuracy: {correct/total*100}%')
+
+train()
