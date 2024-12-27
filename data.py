1f84be7567c6e12c4b354198b88d31d66e357e4b
edwindn <edn.work@proton.me>
2024-12-27 11:57:09 +0000

    Update and rename wav2vec_yesno.py to data.py

--- data.py
+++ data.py
@@ -9,6 +9,8 @@ from torch.utils.data import Dataset, DataLoader
 from torchaudio.models import wav2vec2_base
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+#git clone https://github.com/PerceptiLabs/yesno_voice_recognition
+
 wav2vec = wav2vec2_base().to(device)
 processor = wav2vec.feature_extractor
 encoder = wav2vec.encoder
@@ -85,63 +87,5 @@ for d in test_data:
 dataloader = DataLoader(wav2vec_train, shuffle=True, batch_size=4)
 test_dataloader = DataLoader(wav2vec_test, shuffle=True, batch_size=4)
 
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-
-        self.main = nn.Sequential(
-            nn.Conv2d(1, 16, 8, 4, 2),
-            nn.BatchNorm2d(16),
-            nn.ReLU(),
-            nn.MaxPool2d(2),
-            nn.Conv2d(16, 16, 8, 4, 2),
-            nn.BatchNorm2d(16),
-            nn.ReLU(),
-            nn.MaxPool2d(2),
-            nn.Flatten(),
-            nn.Linear(384, 64),
-            nn.ReLU(),
-            nn.Linear(64, 2),
-            nn.Sigmoid()
-        ).to(device)
-    
-    def forward(self, x):
-        x = x.to(device).unsqueeze(1)
-        return self.main(x)
-    
-net = Net()
-optimizer = torch.optim.Adam(net.parameters(), lr=0.0001)
-loss_fn = nn.CrossEntropyLoss()
-
-def train():
-    for epoch in range(20):
-        torch.cuda.empty_cache()
-
-        print(f'Epoch {epoch}')
-        epoch_loss = 0
-        net.train()
-        for batch in tqdm(dataloader):
-            audio, label = batch
-            audio = audio.to(device)
-            label = label.to(device)
-            preds = net(audio)
-            loss = loss_fn(preds, label)
-            net.zero_grad()
-            loss.backward()
-            optimizer.step()
-            epoch_loss += loss.item()
-        print(f'Loss: {epoch_loss / len(dataloader)}')
-        net.main.eval()
-        correct = 0
-        total = 0
-        for batch in test_dataloader:
-            audio, label = batch
-            audio = audio.to(device)
-            label = label.to(device)
-            preds = net(audio)
-            preds = torch.argmax(preds, dim=1)
-            correct += (preds == label).sum().item()
-            total += len(label)
-        print(f'Accuracy: {correct/total*100}%')
-
-train()
+torch.save(dataloader, 'data/train_loader_wav2vec.pth')
+torch.save(test_dataloader, 'data/test_loader_wav2vec.pth')
